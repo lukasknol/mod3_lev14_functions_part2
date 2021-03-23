780f4ed13bac0840d8dae3d118b338b1bcdde455
Lukas <79308537+lukasknol@users.noreply.github.com>
2021-03-23 18:02:26 +0100

    Add files via upload
    Module 3, Level 14: JavaScript functions

---
+++ script.js
@@ -0,0 +1,24 @@
+// Writing or "defining" a function
+const makePizza = function() {
+   console.log('Get flower');
+   console.log('Add water and yeast');
+   console.log('Kneed');
+   console.log('Let the dough rise');
+   console.log('Top the pizza');
+   console.log('Bake the pizza');
+};
+
+const makeSushi = function() {
+   console.log('Boil the sticky rice');
+   console.log('Chop the fish');
+   console.log('Chop the veggies');
+   console.log('Roll the sushi');
+   console.log('Dip it in the soja');
+};
+
+// Using or "calling" a function
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
